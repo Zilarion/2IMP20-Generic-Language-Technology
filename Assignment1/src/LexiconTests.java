3eaf18229c0cf8bacc9cd8bac07021f8e55cfb59
Ruud Andriessen <r.andriessen@ilysian.com>
2016-04-28 16:18:18 +0200

    Renamed scanner to lexicontests to more accurately represent the task it performs

--- Assignment1/src/LexiconTests.java
+++ Assignment1/src/LexiconTests.java
@@ -1,10 +1,13 @@
 /**
  * Created by ruudandriessen on 28/04/16.
  */
-public class Scanner {
+public class LexiconTests {
     public static void main(String[] args) {
         LexiconQ1 q1 = new LexiconQ1();
         q1.test();
+
+        LexiconQ2 q2 = new LexiconQ2();
+        q2.test();
 
         LexiconJavaComment lexJavaComment = new LexiconJavaComment();
         lexJavaComment.test();
